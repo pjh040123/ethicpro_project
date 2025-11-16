727cce0a7dc74ccc3e5b2d2341358c8707e88643
parkjunhye <pjh040123@naver.com>
2025-11-16 20:40:51 +0900

    commit

--- pages/1_차시별_탐구•실습.py
+++ pages/1_차시별_탐구•실습.py
@@ -36,12 +36,47 @@ with tabs[1]:
 
     with col1:
         with st.expander("**Google Colab 사이트**"):
+            st.write("아래 버튼을 눌러 Google Colab을 실행하세요.")
             st.markdown("""
 <a href="https://colab.research.google.com/drive/1OX_yhe6AhQp1qzj09xgJ1iLzFWv7awNT?authuser=0" target="_blank">
-    <button style="padding:10px 20px; font-size:18px;">코랩 열기</button>
+    <button style="padding:20px 40px; font-size:18px;">Google Colab 열기</button>
 </a>
 """, unsafe_allow_html=True)
+            st.write('')
 
     with col2:
-        with st.expander('코드 실행하기'):
-            st.write('') 
+        with st.expander('**코드 실행하기**'):
+            st.markdown("""
+CPU와 GPU의 처리 방식과 속도를 코딩을 통해 확인해봅시다.
+                        
+코드:
+    
+    import torch
+    import time
+
+    N = 5000
+
+    start = time.time()
+    a = torch.randn(N, N)
+    b = torch.randn(N, N)
+    c = torch.matmul(a, b)
+    cpu_time = time.time() - start
+
+    print(f"✅ CPU 처리시간: {cpu_time:.4f} 초")
+
+    if torch.cuda.is_available():
+        start = time.time()
+        a = torch.randn(N, N, device="cuda")
+        b = torch.randn(N, N, device="cuda")
+        c = torch.matmul(a, b)
+        torch.cuda.synchronize()
+        gpu_time = time.time() - start
+
+        print(f"⚡ GPU 처리시간: {gpu_time:.4f} 초")
+    else:
+        print("❌ GPU를 사용할 수 없습니다. (런타임 > GPU 설정 확인)")
+                        
+‼️주의점‼️
+                        
+    좌측 상단 런타임 → 런타임 유형 변경 → 하드웨어 가속기를 GPU로 변경해줘야 함
+""")
